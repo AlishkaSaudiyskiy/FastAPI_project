80c9535acbbfd33f35343e069dffac1f31e001dc
AlishkaSaudiyskiy <aliy.janibekov.777@mail.ru>
2025-07-02 14:43:32 +0300

    Create base views 'main.py'

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import EmailStr, BaseModel
+
+import uvicorn
+
+from items_views import router as items_router
+from users.views import router as users_router
+
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
+
+
+@app.get('/')
+def hello_index():
+    return {"message": "Hello index!"}
+
+
+@app.get('/hello/')
+def get_hello(name: str):
+    name = name.strip().title()
+    return {"message": f"Hello {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', reload=True)
+    
